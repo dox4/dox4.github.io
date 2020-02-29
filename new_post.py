3f34aa6fa97eb43d0b13cad7072a83bb9ea206b1
dox <1474852655@qq.com>
2020-02-29 17:05:01 +0800

    is it okay that upload a script?

---
+++ new_post.py
@@ -0,0 +1,29 @@
+#! python
+# charset=utf-8
+
+# create a new post with a proper and correct formated title.
+
+import datetime
+import sys
+
+config = '''---
+layout: post
+title:  #title
+date:   #time
+categories: TODO
+---
+'''
+
+def today():
+    today = datetime.datetime.now()
+    return today.strftime("%Y-%m-%d %H:%M:%S")
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('please input a title for your new post:')
+        title = input()
+    else:
+        title = sys.argv[1]
+    t = today()
+    with open('_post/' + t.split(' ')[0] + '-' + title + '.markdown', mode='w', encoding='utf-8') as md:
+        md.write(config.replace('#title', title).replace('#time', t))
